2b26f11143e883f2a4e764ae29721b77257c82fc
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-02-20 23:09:38 -0600

    Create streamlit_test.py

---
+++ StreamlitDeploy/streamlit_test.py
@@ -0,0 +1,10 @@
+# file: streamlit_test.py
+
+import streamlit as st
+
+print('algo')
+
+st.markdown('# Esta es una página de Streamlit')
+st.button('Esto es un botón')
+
+st.text('Se está actualizando?')
